b5f91f6d519dc795ce0fe65ecc63200de0be6dcf
chelsea ip <chelsea@skive.it>
2021-01-25 15:46:58 -0800

    DB

--- resources/views/backend/layouts/footer.blade.php
+++ resources/views/backend/layouts/footer.blade.php
@@ -1,14 +1,14 @@
 <div class="footer">
             <div class="container">
-                <b class="copyright">
+                <b class="copyright">&copy; 2021 Quizio by Chelsea Ip</b>            All rights reserved.
             </div>
         </div>
-        <script src="{{asset('edmin/code/scripts/jquery-1.9.1.min.js" type="text/javascript')}}"></script>
-        <script src="{{asset('edmin/code/scripts/jquery-ui-1.10.1.custom.min.js')}}" type="text/javascript"></script>
-        <script src="{{asset('edmin/code/bootstrap/js/bootstrap.min.js')}}" type="text/javascript"></script>
-        <script src="{{asset('edmin/code/scripts/flot/jquery.flot.js')}}" type="text/javascript"></script>
-        <script src="{{asset('edmin/code/scripts/flot/jquery.flot.resize.js')}}" type="text/javascript"></script>
-        <script src="{{asset('edmin/code/scripts/datatables/jquery.dataTables.js')}}" type="text/javascript"></script>
+        <script src="scripts/jquery-1.9.1.min.js" type="text/javascript"></script>
+        <script src="scripts/jquery-ui-1.10.1.custom.min.js" type="text/javascript"></script>
+        <script src="bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
+        <script src="scripts/flot/jquery.flot.js" type="text/javascript"></script>
+        <script src="scripts/flot/jquery.flot.resize.js" type="text/javascript"></script>
+        <script src="scripts/datatables/jquery.dataTables.js" type="text/javascript"></script>
         <script src="scripts/common.js" type="text/javascript"></script>
       
     </body>
